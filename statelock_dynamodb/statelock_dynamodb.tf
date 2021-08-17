9cdebd2026d401da8d10ea90cc372a9dd2384838
Dinesh23040 <38426080+Dinesh23040@users.noreply.github.com>
2021-08-17 16:08:40 +0530

    Update statelock_dynamodb.tf

--- statelock_dynamodb/statelock_dynamodb.tf
+++ statelock_dynamodb/statelock_dynamodb.tf
@@ -128,7 +128,7 @@ resource "aws_eip" "app_eip"{
 
 resource "aws_eip_association" "app_eip_assoc" {
   instance_id   = aws_instance.cloudcasts_web.id
-  allocation_id = aws_eip.app_eip..allocation_id
+  allocation_id = aws_eip.app_eip.allocation_id
 }
 
 #========================================================================
